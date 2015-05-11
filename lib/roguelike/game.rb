e7405d3deea347ee964741541fe15d50093d94a3
norge <ghrind@gmail.com>
2015-05-11 19:42:53 +0200

    Use the ncursesw gem instead of curses

--- lib/roguelike/game.rb
+++ lib/roguelike/game.rb
@@ -1,22 +1,23 @@
-require 'curses'
+require 'ncursesw'
 require_relative 'grid'
 
 module Roguelike
   # Inspired by https://github.com/aka-bo/ruby-curses-conway
   class Game
     def initialize
-      Curses.init_screen
-      Curses.crmode
-      Curses.curs_set 0
-      Curses.noecho
-      Curses.start_color
+      Ncurses.initscr
+      Ncurses.cbreak
+      Ncurses.curs_set 0
+      Ncurses.noecho
+      Ncurses.start_color
 
-      # Curses.stdscr.keypad(true) # enable arrow keys (required for pageup/down)
+      # enable arrow keys (required for pageup/down)
+      # Ncurses.keypad(scr, true) 
 
-      Curses.init_pair(Curses::COLOR_BLACK, Curses::COLOR_BLACK, Curses::COLOR_BLACK)
-      Curses.init_pair(Curses::COLOR_BLUE, Curses::COLOR_BLACK, Curses::COLOR_BLUE)
-      Curses.init_pair(Curses::COLOR_RED, Curses::COLOR_RED, Curses::COLOR_BLACK)
-      Curses.init_pair(Curses::COLOR_GREEN, Curses::COLOR_GREEN, Curses::COLOR_BLACK)
+      Ncurses.init_pair(Ncurses::COLOR_BLACK, Ncurses::COLOR_BLACK, Ncurses::COLOR_BLACK)
+      Ncurses.init_pair(Ncurses::COLOR_RED, Ncurses::COLOR_RED, Ncurses::COLOR_BLACK)
+      Ncurses.init_pair(Ncurses::COLOR_GREEN, Ncurses::COLOR_GREEN, Ncurses::COLOR_BLACK)
+      Ncurses.init_pair(Ncurses::COLOR_WHITE, Ncurses::COLOR_WHITE, Ncurses::COLOR_BLACK)
     end
 
     # @param map_filename [String] The filename of a map
@@ -32,19 +33,21 @@ module Roguelike
 
       while true
         begin
+          # TODO redraw only if something happened
           win = draw grid
           command = command_from_key_combination(win.getch)
           grid.tick(command)
         rescue Interrupt
           break
         ensure
-          win.close if win
+          win.del if win
+          Ncurses.echo
+          Ncurses.nl
+          Ncurses.endwin
         end
       end
 
-      Curses.clear
-      Curses.refresh
-      Curses.close_screen
+
       puts 'Goodbye...'
     end
 
@@ -53,67 +56,59 @@ module Roguelike
     def show_message(message)
       padding = 6
       width = message.length + padding
-      win = Curses::Window.new(5, width, (Curses.lines - 5)/2, (Curses.cols - width)/2)
-      win.box '|', '-'
-      win.setpos 2, 3
-      win.addstr message
+      win = Ncurses::WINDOW.new(5, width, (Ncurses.LINES - 5)/2, (Ncurses.COLS - width)/2)
+      win.border *([0]*8)
+      win.mvaddstr 2, 3, message
 
       win.getch
-      win.close
+      win.del
     end
 
     def draw(grid)
-      Curses.doupdate
-      win = Curses::Window.new Curses.lines, Curses.cols, 0, 0
+      win = Ncurses::WINDOW.new Ncurses.LINES, Ncurses.COLS, 0, 0
+      win.border *([0]*8)
 
-      delta_x = (Curses.cols - 2) / 2
-      delta_y = (Curses.lines - 2) / 2
+      delta_x = (Ncurses.LINES - 2) / 2
+      delta_y = (Ncurses.COLS - 2) / 2
 
       center = grid.player
 
       grid.all_cells.each do |c|
         next if (center[:x] - c.x).abs > delta_x || (center[:y] - c.y).abs > delta_y
         if c.wall
-          add_cell(win, c.x - center[:x] + delta_x, c.y - center[:y] + delta_y, '#', Curses::COLOR_BLACK)
+          add_cell(win, c.x - center[:x] + delta_x, c.y - center[:y] + delta_y, '#', Ncurses::COLOR_BLACK)
         else
-          add_cell(win, c.x - center[:x] + delta_x, c.y - center[:y] + delta_y, '.', Curses::COLOR_BLACK)
+          add_cell(win, c.x - center[:x] + delta_x, c.y - center[:y] + delta_y, '.', Ncurses::COLOR_BLACK)
         end
       end
 
       if grid.path
         grid.path.each do |c|
           next if (center[:x] - c.x).abs > delta_x || (center[:y] - c.y).abs > delta_y
-          add_cell win, c.x - center[:x] + delta_x, c.y - center[:y] + delta_y, '.', Curses::COLOR_GREEN
+          add_cell win, c.x - center[:x] + delta_x, c.y - center[:y] + delta_y, '.', Ncurses::COLOR_GREEN
         end
       end
 
       grid.stalkers.each do |c|
         next if (center[:x] - c[:x]).abs > delta_x || (center[:y] - c[:y]).abs > delta_y
-        add_cell win, c[:x] - center[:x] + delta_x, c[:y] - center[:y] + delta_y, 's', Curses::COLOR_GREEN
+        add_cell win, c[:x] - center[:x] + delta_x, c[:y] - center[:y] + delta_y, 's', Ncurses::COLOR_GREEN
       end
 
-      add_cell win, delta_x, delta_y, '@', Curses::COLOR_RED
+      add_cell win, delta_x, delta_y, '@', Ncurses::COLOR_RED
 
-      win.setpos 0, 0
-      win.addstr "x: #{grid.player[:x]} y: #{grid.player[:y]}"
-
-      win.refresh
+      win.color_set Ncurses::COLOR_WHITE, nil
+      win.mvaddstr 1, 1, "x: #{grid.player[:x]} y: #{grid.player[:y]}"
 
       win
     end
 
-    def add_cell(win, x, y, symbol, color = Curses::COLOR_BLUE)
-      win.setpos y, x
-      color = Curses.color_pair color
-      win.attron(color|Curses::A_NORMAL) {
-        win.addstr symbol
-      }
-
-      Curses.doupdate
+    def add_cell(win, x, y, symbol, color)
+      win.color_set color, nil
+      win.mvaddstr x, y, symbol
     end
 
     def command_from_key_combination(key)
-      case key
+      case key.chr
       when ' '
         'player.wait'
       else
